dd6c91bd067283c8f8fad5a2a4e7a63dde7d55dd
Hunter Fox <36008636+hunterjfox@users.noreply.github.com>
2025-06-11 17:14:11 -0500

    home hero js

--- js/acme.js
+++ js/acme.js
@@ -2,7 +2,64 @@
 
 //May split if required
 
-$(document).foundation();;/*------------------------------------*\
+$(document).foundation();
+
+//Homepage hero accordions
+
+const accordionTitles = [{
+        id: "brain-health",
+        label: "brain health"
+    },
+    {
+        id: "leading-research",
+        label: "cancer research"
+    },
+    {
+        id: "treat-diseases",
+        label: "infectious disease treatment"
+    },
+    {
+        id: "metabolic-health",
+        label: "metabolic health"
+    },
+    {
+        id: "title-here",
+        label: "title lorem ipsum"
+    },
+]
+
+document.addEventListener('click', heroAccordions);
+
+function heroAccordions() {
+
+    accordionTitles.forEach(item => {
+        const element = document.getElementById(item.id);
+
+        if (element) {
+
+            const parentWithAria = element.closest('[aria-hidden="false"]');
+
+            if (parentWithAria) {
+                const heroText = document.querySelector('span.highlight');
+
+                if (heroText) {
+                    heroText.textContent = item.label;
+                }
+            }
+        }
+        return;
+
+    });
+}
+
+
+
+
+
+function isDisplayed(element) {
+    const style = window.getComputedStyle(element);
+    return style.display !== 'none';
+};/*------------------------------------*\
     #PRIMARY NAVIGATION
 \*------------------------------------*/
 /**
